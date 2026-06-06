ee24c21565e705ff34ec385074cbfedb9288c3b4
srevathi <srevathianandraj@gmail.com>
2026-06-06 21:35:50 +0530

    1st app

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,14 @@
-
 fun main() {
-    println("Shrivatsan the great")
+    var isNameVisible = false
+    println("--- App Started ---")
+    println("Button available: [Show]")
+
+    // Simulate clicking the button
+    println("\n[User clicks the 'Show' button]")
+    isNameVisible = true
+
+    // App updates the UI
+    if (isNameVisible) {
+        println("App displays: Shrivatsan")
+    }
 }
